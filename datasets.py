144a98312a85461ad9c5b948a80702a062b8db1b
Marshall Lee <marsh92909@gmail.com>
2022-09-04 15:11:02 -0400

    Add functions for reading coords, outputs as nodes

--- datasets.py
+++ datasets.py
@@ -12,6 +12,9 @@ import torch
 from torch.utils.data import Dataset, Subset, DataLoader
 
 
+DATASET_FOLDER = 'Training_Data_50'
+
+
 class LatticeDataset(Dataset):
     def __init__(self, folder: str) -> None:
         with open(os.path.join(folder, 'cache.pickle'), 'rb') as f:
@@ -24,8 +27,23 @@ class LatticeDataset(Dataset):
         return self.inputs[index, ...], self.outputs[index, ...]
 
 
+def read_coordinates() -> np.ndarray:
+    """Return a 3D array of node numbers."""
+    
+    with open(os.path.join(DATASET_FOLDER, 'InputCoords.txt'), 'r') as f:
+        lines = f.readlines()[1:]
+    
+    nodes = np.zeros((51, 51, 51), dtype=np.int32)
+    for number, (x, y, z) in enumerate(lines, 1):
+        nodes[x, y, z] = number
+    assert np.all(nodes > 0)
+
+    return nodes
+
 def read_struts() -> List[List[int]]:
-    with open('Training_Data_50/Struts.txt', 'r') as f:
+    """Return a list of lists of the two nodes for each strut."""
+
+    with open(os.path.join(DATASET_FOLDER, 'Struts.txt'), 'r') as f:
         # Remove the header line.
         lines = f.readlines()[1:]
     
@@ -38,9 +56,9 @@ def read_struts() -> List[List[int]]:
     return struts
 
 def read_inputs() -> np.ndarray:
-    """Return input images as a single 5D array with shape (number of samples, 1 channel, height, width, depth)."""
+    """Return input images as a 5D array with shape (number of samples, 1 channel, height, width, depth)."""
     
-    directory = 'Training_Data_50/Input_Data'
+    directory = os.path.join(DATASET_FOLDER, 'Input_Data')
     folders = next(os.walk(directory))[1]
     folders.sort(key=lambda folder: int(folder.split('_')[1]))
 
@@ -62,9 +80,9 @@ def read_inputs() -> np.ndarray:
     return inputs
 
 def read_outputs() -> np.ndarray:
-    """Return output data as a single 3D array with shape (number of samples, height of the adjacency matrix, width of the adjacency matrix)."""
+    """Return output data as a 3D array with shape (number of samples, height of the adjacency matrix, width of the adjacency matrix)."""
     
-    directory = 'Training_Data_50/Output_Data'
+    directory = os.path.join(DATASET_FOLDER, 'Output_Data')
 
     files = glob.glob(os.path.join(directory, '*.txt'))
     files.sort(key=lambda file: int(os.path.basename(file).split('.')[0].split('_')[1]))
@@ -105,34 +123,51 @@ def read_outputs() -> np.ndarray:
 
     return outputs
 
-inputs = read_inputs()
-with open('inputs.pickle', 'wb') as f:
-    pickle.dump(inputs, f)
-outputs = read_outputs()
-with open('outputs.pickle', 'wb') as f:
-    pickle.dump(outputs, f)
+def read_outputs_as_nodes() -> np.ndarray:
+    """Return output data as a 5D array of 1s indicating nodes with shape (number of samples, 1 channel, height, width, depth)."""
+    
+    directory = os.path.join(DATASET_FOLDER, 'Output_Data')
+
+    files = glob.glob(os.path.join(directory, '*.txt'))
+    files.sort(key=lambda file: int(os.path.basename(file).split('.')[0].split('_')[1]))
+
+    struts = read_struts()
+    node_numbers = read_coordinates()
+
+    nodes = set()
+
+    data_type = np.uint8
+    outputs = np.zeros([len(files), 1, 51, 51, 51], dtype=data_type)
+
+    for i, file in enumerate(files):
+        print(f"Reading file {file}...", end='\r')
+
+        with open(file, 'r') as f:
+            # Ignore the header line.
+            _ = f.readline()
+            # Read all lines except the lines at the bottom containing duplicate struts.
+            for line in range(1, h*w + 1):
+                strut, d = f.readline().strip().split(',')
+                strut, d = int(strut), float(d)
+
+                if d != 0:
+                    node_1, node_2 = struts[strut - 1]
+                    nodes.add(node_1)
+                    nodes.add(node_2)
+
+    for node in nodes:
+        outputs[node_numbers == node] = 1
+    
+    outputs = torch.tensor(outputs)
+
+    return outputs
 
 
-# if __name__ == "__main__":
-#     file = "Training_Data_50/Struts.txt"
-#     with open(file, 'r') as f:
-#         for i, line in enumerate(f.readlines()):
-#             if '116420,119021' in line:
-#                 print(i)
-
-#         # # print(lines[2957942])
-#         # print(lines[3094521])
-
-#     # folder = "Training_Data_50/Output_Data"
-#     # files = glob.glob(os.path.join(folder, "*.txt"))
-#     # files.sort()
-#     # for file in files[:1]:
-#     #     print(file, end='\r')
-#     #     with open(file, 'r') as f:
-#     #         lines = f.readlines()[1:]
-#     #         lines = lines[(49**3 * 26):]
-#     #         for line in lines:
-#     #             if '.' in line.strip().split(',')[1]:
-#     #                 print(line)
-#     #         # if any('.' in line.strip().split(',')[1] for line in lines):
-#     #         #     print(f"redundant diameters found in {file}")
+if __name__ == "__main__":
+    inputs = read_inputs()
+    with open('inputs.pickle', 'wb') as f:
+        pickle.dump(inputs, f)
+    
+    outputs = read_outputs()
+    with open('outputs.pickle', 'wb') as f:
+        pickle.dump(outputs, f)
