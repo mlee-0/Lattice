Convert outputs to vector form; fix normalization
- Adjacency matrix contains duplicates & high sparsity
- Add conversion of vector data to lattice in vis
- Shortened model names & changed Dataset class names
- Only normalize input image values > -1; prevents -1 from becoming pos

datasets.py
@@ -1,14 +1,6 @@
 """Dataset classes that load previously cached dataset files."""
 
 
-import os
-import pickle
-import time
-
-import torch
-import torch_geometric
-
-
 try:
     from google.colab import drive  # type: ignore
 except ImportError:
@@ -17,8 +9,17 @@ else:
     drive.mount('/content/drive')
     DATASET_FOLDER = 'drive/My Drive/Lattice'
 
+import os
+import pickle
+import time
 
-class CnnDataset(torch.utils.data.Dataset):
+import torch
+import torch_geometric
+
+from preprocessing import read_pickle
+
+
+class AdjacencyDataset(torch.utils.data.Dataset):
     def __init__(self, count: int=None) -> None:
         """
         `count`: The number of data to use, or None to use the entire dataset.
@@ -26,10 +27,8 @@ class CnnDataset(torch.utils.data.Dataset):
 
         time_start = time.time()
         
-        with open(os.path.join(DATASET_FOLDER, 'inputs.pickle'), 'rb') as f:
-            self.inputs = pickle.load(f).float()
-        with open(os.path.join(DATASET_FOLDER, 'outputs.pickle'), 'rb') as f:
-            self.outputs = pickle.load(f).float()
+        self.inputs = read_pickle(os.path.join(DATASET_FOLDER, 'inputs.pickle')).float()
+        self.outputs = read_pickle(os.path.join(DATASET_FOLDER, 'outputs.pickle')).float()
         
         if count is not None:
             self.inputs = self.inputs[:count, ...]
@@ -38,8 +37,8 @@ class CnnDataset(torch.utils.data.Dataset):
         assert self.inputs.shape[0] == self.outputs.shape[0]
 
         # Normalize input data to have zero mean and unit variance.
-        self.inputs -= self.inputs.mean()
-        self.inputs /= self.inputs.std()
+        self.inputs[self.inputs >= 0] -= self.inputs[self.inputs >= 0].mean()
+        self.inputs[self.inputs >= 0] /= self.inputs[self.inputs >= 0].std()
 
         time_end = time.time()
         print(f"Loaded dataset in {round(time_end - time_start)} seconds.")
@@ -50,7 +49,30 @@ class CnnDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         return self.inputs[index, ...], self.outputs[index, ...]
 
-class GnnDataset(torch_geometric.data.Dataset):
+class VectorDataset(torch.utils.data.Dataset):
+    def __init__(self, count: int=None) -> None:
+        super().__init__()
+
+        self.inputs = read_pickle(os.path.join(DATASET_FOLDER, 'inputs.pickle')).float()
+        self.outputs = read_pickle(os.path.join(DATASET_FOLDER, 'outputs_vector.pickle'))
+
+        if count is not None:
+            self.inputs = self.inputs[:count, ...]
+            self.outputs = self.outputs[:count, ...]
+        
+        assert self.inputs.shape[0] == self.outputs.shape[0]
+
+        # Normalize input data to have zero mean and unit variance.
+        self.inputs[self.inputs >= 0] -= self.inputs[self.inputs >= 0].mean()
+        self.inputs[self.inputs >= 0] /= self.inputs[self.inputs >= 0].std()
+
+    def __len__(self) -> int:
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        return self.inputs[index, ...], self.outputs[index, ...]
+
+class GraphDataset(torch_geometric.data.Dataset):
     def __init__(self, count: int=None) -> None:
         with open(os.path.join(DATASET_FOLDER, 'graphs.pickle'), 'rb') as f:
             self.dataset = pickle.load(f)

main.py
@@ -13,6 +13,7 @@ import torch_geometric
 from datasets import *
 import metrics
 from models import *
+from visualization import *
 
 
 def plot_loss(figure, epochs: list, loss: List[list], labels: List[str], start_epoch: int = None) -> None:
@@ -29,15 +30,13 @@ def plot_loss(figure, epochs: list, loss: List[list], labels: List[str], start_e
     axis = figure.add_subplot(1, 1, 1)  # Number of rows, number of columns, index
     
     # markers = (".:", ".-")
-    # colors = (Colors.BLUE, Colors.ORANGE)
 
     # Plot each set of loss values.
     for i, loss_i in enumerate(loss):
         if not len(loss_i):
             continue
-        # color = colors[i % len(colors)]
         axis.plot(epochs[:len(loss_i)], loss_i, ".-", label=labels[i])
-        axis.annotate(f"{loss_i[-1]:,.2f}", (epochs[-1 - (len(epochs)-len(loss_i))], loss_i[-1]), fontsize=10)
+        axis.annotate(f"{loss_i[-1]:,.2e}", (epochs[-1 - (len(epochs)-len(loss_i))], loss_i[-1]), fontsize=10)
     
     # # Plot a vertical line indicating when the current training session began.
     # if start_epoch:
@@ -458,6 +457,17 @@ def main(
 
         if evaluate:
             results = evaluate_regression(outputs, labels, inputs, dataset, queue=queue, info_gui=info_gui)
+        
+        # lattice = convert_vector_to_lattice(outputs[0, ...])
+        # visualize_lattice(*lattice)
+
+        # metrics.plot_error_by_label(outputs, labels)
+        # lattice = convert_adjacency_to_lattice(outputs[0, ...])
+        # visualize_lattice(*lattice)
+
+        # h = 0
+        # for i in range(5):
+        #     metrics.plot_adjacency(outputs[i, h:h+500, :], labels[i, h:h+500, :])
 
 
 if __name__ == "__main__":
@@ -466,14 +476,14 @@ if __name__ == "__main__":
         "train_existing": not True,
         "save_model_every": 5,
 
-        "epoch_count": 10,
+        "epoch_count": 5,
         "learning_rate": 1e-3,
         "decay_learning_rate": not True,
-        "batch_sizes": (16, 64, 64),
+        "batch_sizes": (32, 64, 64),
         "training_split": (0.8, 0.1, 0.1),
         
-        "dataset": GnnDataset(),
-        "Model": LatticeGnn,
+        "dataset": VectorDataset(),
+        "Model": ResNet,
         "Optimizer": torch.optim.Adam,
         "Loss": nn.MSELoss,
         

models.py
@@ -10,7 +10,7 @@ def get_parameter_count(model: torch.nn.Module) -> int:
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-class LatticeCnn(torch.nn.Module):
+class Cnn(torch.nn.Module):
     """3D CNN whose input is a 3D array of densities and whose output is a 1D array of strut diameters."""
 
     def __init__(self, device: str='cpu') -> None:
@@ -34,8 +34,9 @@ class LatticeCnn(torch.nn.Module):
 
         # Output size, representing the maximum number of struts possible.
         self.shape_output = (
-            int(h * w * d),
-            int(strut_neighborhood ** 3 - 1),
+            16309,
+            # int(h * w * d),
+            # int(strut_neighborhood ** 3 - 1),
         )
         size_output = int(np.prod(self.shape_output))
 
@@ -77,7 +78,7 @@ class LatticeCnn(torch.nn.Module):
 
         return x
 
-class LatticeResNet(torch.nn.Module):
+class ResNet(torch.nn.Module):
     """3D ResNet-based CNN whose input is a 3D array of densities and whose output is a 1D array of strut diameters."""
     def __init__(self, device: str='cpu') -> None:
         super().__init__()
@@ -96,8 +97,9 @@ class LatticeResNet(torch.nn.Module):
 
         # Output size, representing the maximum number of struts possible.
         self.shape_output = (
-            int(h * w * d),
-            int(strut_neighborhood ** 3 - 1),
+            16309,
+        #     int(h * w * d),
+        #     int(strut_neighborhood ** 3 - 1),
         )
         size_output = int(np.prod(self.shape_output))
 
@@ -147,8 +149,8 @@ class LatticeResNet(torch.nn.Module):
 
         return x
 
-class LatticeGnn(torch.nn.Module):
-    """A GNN whose input is a fully-connected graph of node locations and whose output is a graph of strut diameters."""
+class Gnn(torch.nn.Module):
+    """GNN whose input is a graph of node densities and coordinates and whose output is a 2D tensor of strut diameters with shape (number of struts, 1)."""
 
     def __init__(self, device: str='cpu') -> None:
         super().__init__()

preprocessing.py
@@ -66,7 +66,7 @@ def read_struts() -> List[Tuple[int, int]]:
     return struts
 
 def read_inputs(count: int=None) -> torch.tensor:
-    """Return input images as a 5D tensor with shape (number of samples, 1 channel, height, width, depth)."""
+    """Return input images as a 5D tensor with shape (number of data, 1 channel, height, width, depth)."""
     
     directory = os.path.join(DATASET_FOLDER, 'Input_Data')
     folders = next(os.walk(directory))[1]
@@ -129,46 +129,17 @@ def read_outputs(count: int=None) -> List[List[Tuple[int, int]]]:
 
     return outputs
 
-def convert_outputs_to_adjacency(outputs: list) -> np.ndarray:
-    """Convert the given output data to a 3D array with shape (number of samples, height of the adjacency matrix, width of the adjacency matrix)."""
+def get_unique_strut_numbers(struts: list) -> list:
+    """Return a list of strut numbers corresponding only to node numbers in increasing order (for example, (1, 2) but not (2, 1))."""
 
-    # Number of data.
-    n = len(outputs)
+    unique_struts = {tuple(sorted(strut)) for strut in struts}
 
-    # Height of the adjacency matrix, equal to the number of nodes.
-    h = int(np.prod(INPUT_SHAPE))
-    # Width of the adjacency matrix, equal to the number of struts per node. Excludes some struts to avoid redundancy.
-    w = STRUT_NEIGHBORHOOD ** 3 - 1
-
-    node_numbers = make_node_numbers()
-    struts = read_struts()
-
-    # Dictionary of node indices {node: (x, y, z)} to reduce runtime by avoiding search.
-    node_indices = {node_numbers[x, y, z]: (x, y, z) for x in range(node_numbers.shape[0]) for y in range(node_numbers.shape[1]) for z in range(node_numbers.shape[2])}
-
-    adjacency = torch.zeros((n, h, w), dtype=torch.float32)
-
-    for i, output in enumerate(outputs):
-        print(f"Converting output {i+1} of {n}...", end='\r')
-
-        for strut, d in output:
-            node_1, node_2 = struts[strut - 1]
-
-            x1, y1, z1 = node_indices[node_1]
-            x2, y2, z2 = node_indices[node_2]
-
-            # Indices increase along Z first, Y second, X last.
-            row = np.ravel_multi_index((x1, y1, z1), node_numbers.shape)
-            column = np.ravel_multi_index((x2-x1+1, y2-y1+1, z2-z1+1), (STRUT_NEIGHBORHOOD,)*3)
-            if column >= 13:
-                # Subtract indices greater than that of the node at the center of the 3x3x3 neighborhood.
-                column -= 1
-
-            adjacency[i, row, column] = d
-
-    print(f"\nDensity of adjacency matrix: {(adjacency > 0).sum() / adjacency.numel()}")
-
-    return adjacency
+    strut_numbers = []
+    for number, strut in enumerate(struts, 1):
+        if strut in unique_struts:
+            strut_numbers.append(number)
+    
+    return strut_numbers
 
 def apply_mask_inputs(inputs: torch.tensor, outputs: list):
     """Replace density values outside the predefined volume of space with some constant value."""
@@ -255,7 +226,9 @@ def augment_outputs(outputs: list, augmentations: int=None):
         node_indices = {rotated_node_numbers[x, y, z]: (x, y, z) for x in range(rotated_node_numbers.shape[0]) for y in range(rotated_node_numbers.shape[1]) for z in range(rotated_node_numbers.shape[2])}
 
         for i, output in enumerate(outputs):
-            print(f"Augmenting output {i} of {len(outputs)} for rotation {rotation}...", end='\r')
+            if i % 10 == 0:
+                print(f"Augmenting output {i} of {len(outputs)} for rotation {rotation}...", end='\r')
+
             rotated_output = [None] * len(output)
             for j, (strut, diameter) in enumerate(output):
                 node_1, node_2 = struts[strut - 1]
@@ -273,6 +246,72 @@ def augment_outputs(outputs: list, augmentations: int=None):
             rotated_outputs.append(rotated_output)
 
     return rotated_outputs
+
+def convert_outputs_to_adjacency(outputs: list) -> np.ndarray:
+    """Convert the given output data to a 3D array with shape (number of data, height of the adjacency matrix, width of the adjacency matrix)."""
+
+    # Number of data.
+    n = len(outputs)
+
+    # Height of the adjacency matrix, equal to the number of nodes.
+    h = int(np.prod(INPUT_SHAPE))
+    # Width of the adjacency matrix, equal to the number of struts per node. Excludes some struts to avoid redundancy.
+    w = STRUT_NEIGHBORHOOD ** 3 - 1
+
+    node_numbers = make_node_numbers()
+    struts = read_struts()
+
+    # Dictionary of node indices {node: (x, y, z)} to reduce runtime by avoiding search.
+    node_indices = {node_numbers[x, y, z]: (x, y, z) for x in range(node_numbers.shape[0]) for y in range(node_numbers.shape[1]) for z in range(node_numbers.shape[2])}
+
+    adjacency = torch.zeros((n, h, w), dtype=torch.float32)
+
+    for i, output in enumerate(outputs):
+        if i % 100 == 0:
+            print(f"Converting output {i+1} of {n}...", end='\r')
+
+        for strut, d in output:
+            node_1, node_2 = struts[strut - 1]
+
+            x1, y1, z1 = node_indices[node_1]
+            x2, y2, z2 = node_indices[node_2]
+
+            # Indices increase along Z first, Y second, X last.
+            row = np.ravel_multi_index((x1, y1, z1), node_numbers.shape)
+            column = np.ravel_multi_index((x2-x1+1, y2-y1+1, z2-z1+1), (STRUT_NEIGHBORHOOD,)*3)
+            if column >= 13:
+                # Subtract indices greater than that of the node at the center of the 3x3x3 neighborhood.
+                column -= 1
+
+            adjacency[i, row, column] = d
+
+    print(f"\nDensity of adjacency matrix: {(adjacency > 0).sum() / adjacency.numel()}")
+
+    return adjacency
+
+def convert_outputs_to_vector(outputs: list) -> np.ndarray:
+    """Convert the given output data to a 2D array of diameters with shape (number of data, number of struts). Duplicate struts are excluded."""
+
+    # Number of data.
+    n = len(outputs)
+
+    strut_numbers = get_unique_strut_numbers(read_struts())
+    # Dictionary of indices {strut number: index} to reduce runtime by avoiding list search.
+    strut_numbers_indices = {number: index for index, number in enumerate(strut_numbers)}
+
+    vector = torch.zeros((n, len(strut_numbers)), dtype=torch.float32)
+
+    for i, output in enumerate(outputs):
+        if i % 100 == 0:
+            print(f"Converting output {i+1} of {n}...", end='\r')
+
+        strut_indices = torch.tensor([strut_numbers_indices[_[0]] for _ in output])
+        diameters = torch.tensor([_[1] for _ in output])
+        vector[i, strut_indices] = diameters
+
+    print(f"\nDensity of vector: {(vector > 0).sum() / vector.numel()}")
+
+    return vector
 
 def convert_dataset_to_graph(inputs: np.ndarray, outputs: list) -> List[torch_geometric.data.Data]:
     """Convert a 5D array of input data and a list of output data to a list of graphs."""
@@ -320,7 +359,7 @@ def convert_dataset_to_graph(inputs: np.ndarray, outputs: list) -> List[torch_ge
                     edge_index.add(tuple(sorted((node_1, node_2))))
         
         edge_index = list(edge_index)
-        # Dictionary of strut indices {(node 1, node 2): indices} to reduce runtime by avoiding list search.
+        # Dictionary of strut indices {(node 1, node 2): index} to reduce runtime by avoiding list search.
         edge_index_indices = {nodes: index for index, nodes in enumerate(edge_index)}
         # Each strut must be represented by two separate edges in opposite directions to make the graph undirected (both (1, 2) and (2, 1)).
         edge_index.extend([nodes[::-1] for nodes in edge_index])
@@ -357,25 +396,30 @@ def mask_of_active_nodes(strut_numbers: list, struts: list, node_numbers: np.nda
 def read_pickle(path: str) -> Any:
     with open(path, 'rb') as f:
         data = pickle.load(f)
-    print(f"Loaded {type(data)} from {path}.")
+    print(f"Loaded {type(data)} with length {len(data)} from {path}.")
     return data
 
 def write_pickle(data: Any, path: str) -> None:
     with open(path, 'wb') as f:
         pickle.dump(data, f)
-    print(f"Saved {type(data)} to {path}.")
+    print(f"Saved {type(data)} with length {len(data)} to {path}.")
 
 
 if __name__ == "__main__":
-    inputs = read_inputs()
-    outputs = read_outputs()
-    inputs = augment_inputs(inputs)
-    outputs = augment_outputs(outputs)
+    # inputs = read_inputs()
+    # outputs = read_outputs()
+    # inputs = augment_inputs(inputs)
+    # outputs = augment_outputs(outputs)
 
-    masked_inputs = apply_mask_inputs(inputs, outputs)
-    adjacency = convert_outputs_to_adjacency(outputs)
-    write_pickle(masked_inputs, 'Training_Data_10/inputs.pickle')
-    write_pickle(adjacency, 'Training_Data_10/outputs.pickle')
+    # masked_inputs = apply_mask_inputs(inputs, outputs)
+    # adjacency = convert_outputs_to_adjacency(outputs)
+    # write_pickle(masked_inputs, 'Training_Data_10/inputs.pickle')
+    # write_pickle(adjacency, 'Training_Data_10/outputs.pickle')
     
-    graphs = convert_dataset_to_graph(inputs, outputs)
-    write_pickle(graphs, 'Training_Data_10/graphs.pickle')
+    # graphs = convert_dataset_to_graph(inputs, outputs)
+    # write_pickle(graphs, 'Training_Data_10/graphs.pickle')
+
+    outputs = read_outputs()
+    outputs = augment_outputs(outputs)
+    vector = convert_outputs_to_vector(outputs)
+    write_pickle(vector, 'Training_Data_10/outputs_vector.pickle')

visualization.py
@@ -148,14 +148,32 @@ def convert_adjacency_to_lattice(array: np.ndarray) -> Tuple[list, list, list]:
                 
                 # Skip duplicate struts.
                 strut = tuple(sorted((node_1, node_2)))
-                if strut in unique_struts:
-                    continue
-                else:
+                if strut not in unique_struts:
                     unique_struts.add(strut)
 
-                coordinates_1.append(tuple(coordinates[node_1 - 1]))
-                coordinates_2.append(tuple(coordinates[node_2 - 1]))
-                diameters.append(diameter)
+                    coordinates_1.append(tuple(coordinates[node_1 - 1]))
+                    coordinates_2.append(tuple(coordinates[node_2 - 1]))
+                    diameters.append(diameter)
+    
+    return coordinates_1, coordinates_2, diameters
+
+def convert_vector_to_lattice(vector: np.ndarray) -> Tuple[list, list, list]:
+    """Convert a 1D array into a tuple of coordinates and diameters."""
+    
+    struts = read_struts()
+    coordinates = read_coordinates()
+    strut_numbers = get_unique_strut_numbers(struts)
+
+    coordinates_1, coordinates_2, diameters = [], [], []
+
+    for i, diameter in enumerate(vector):
+        if diameter > 0:
+            node_1, node_2 = struts[strut_numbers[i] - 1]
+            assert node_1 < node_2
+
+            coordinates_1.append(tuple(coordinates[node_1 - 1]))
+            coordinates_2.append(tuple(coordinates[node_2 - 1]))
+            diameters.append(diameter)
     
     return coordinates_1, coordinates_2, diameters
 
@@ -193,10 +211,11 @@ def visualize_lattice(locations_1: List[Tuple[float, float, float]], locations_2
         line.SetResolution(0)
         line.Update()
 
+        radius = diameter / 2
         tube = vtk.vtkTubeFilter()
         tube.SetInputData(line.GetOutput())
-        tube.SetRadius(diameter / 2)
-        tube.SetNumberOfSides(3)
+        tube.SetRadius(radius)
+        tube.SetNumberOfSides(5)
         
         mapper = vtk.vtkPolyDataMapper()
         mapper.SetInputConnection(tube.GetOutputPort())
@@ -214,12 +233,17 @@ def visualize_lattice(locations_1: List[Tuple[float, float, float]], locations_2
 
 
 if __name__ == "__main__":
-    # lattice = convert_output_to_lattice(read_outputs(1)[0])
+    # lattice = convert_output_to_lattice(read_outputs(3)[2])
     # visualize_lattice(*lattice)
 
-    with open('Training_Data_10/outputs.pickle', 'rb') as f:
-        outputs = pickle.load(f)
-    lattice = convert_adjacency_to_lattice(np.array(outputs[0, :, :]))
+    # with open('Training_Data_10/outputs.pickle', 'rb') as f:
+    #     outputs = pickle.load(f)
+    # lattice = convert_adjacency_to_lattice(np.array(outputs[0, :, :]))
+    # visualize_lattice(*lattice)
+    
+    with open('Training_Data_10/outputs_vector.pickle', 'rb') as f:
+        vector = pickle.load(f)
+    lattice = convert_vector_to_lattice(vector[1000, :])
     visualize_lattice(*lattice)
 
     # with open("Training_Data_10/graphs.pickle", 'rb') as f: